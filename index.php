2e4781769200b6f94986759b0dda03cd55287a25
LucinoMatos <35868481+LucinoMatos@users.noreply.github.com>
2018-01-27 11:19:09 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,8 @@
 <?php
 
-
-<<<<<<< HEAD
-  echo "Olá Mundo 2017";
-
-  echo "Udemy!";
-=======
   echo "Olá Mundo 2018";
    
   echo "hey guys";
->>>>>>> 388d9b85df3d1eec8ab190f2c04a49c5610af9cd
+
 
   ?>
